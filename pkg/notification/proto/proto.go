361bad2dfde532f2fb10ccf533888b4a9222186c
Johnathan Falk <johnathan.falk@gmail.com>
2025-07-22 23:30:43 -0400

    feat(notification): implement service protobufs

--- pkg/notification/proto/proto.go
+++ pkg/notification/proto/proto.go
@@ -7,19 +7,29 @@ package notificationpb
 import (
 	notepb_enums "github.com/jdfalk/gcommon/pkg/notification/proto/enums"
 	notepb_messages "github.com/jdfalk/gcommon/pkg/notification/proto/messages"
+	notepb_requests "github.com/jdfalk/gcommon/pkg/notification/proto/requests"
+	notepb_responses "github.com/jdfalk/gcommon/pkg/notification/proto/responses"
+	notepb_services "github.com/jdfalk/gcommon/pkg/notification/proto/services"
 )
 
 // Message types
 type (
-	NotificationMessage     = notepb_messages.NotificationMessage
-	DeliveryChannel         = notepb_messages.DeliveryChannel
-	Template                = notepb_messages.Template
-	SubscriptionPreferences = notepb_messages.SubscriptionPreferences
-	EventNotification       = notepb_messages.EventNotification
+	NotificationMessage       = notepb_messages.NotificationMessage
+	DeliveryChannel           = notepb_messages.DeliveryChannel
+	Template                  = notepb_messages.Template
+	SubscriptionPreferences   = notepb_messages.SubscriptionPreferences
+	EventNotification         = notepb_messages.EventNotification
+	SendNotificationRequest   = notepb_requests.SendNotificationRequest
+	UpdatePreferencesRequest  = notepb_requests.UpdatePreferencesRequest
+	GetTemplateRequest        = notepb_requests.GetTemplateRequest
+	SendNotificationResponse  = notepb_responses.SendNotificationResponse
+	UpdatePreferencesResponse = notepb_responses.UpdatePreferencesResponse
+	GetTemplateResponse       = notepb_responses.GetTemplateResponse
 )
 
 // Enum types
 type (
-	DeliveryStatus      = notepb_enums.DeliveryStatus
-	DeliveryChannelType = notepb_messages.DeliveryChannelType
+	DeliveryStatus            = notepb_enums.DeliveryStatus
+	DeliveryChannelType       = notepb_messages.DeliveryChannelType
+	NotificationServiceServer = notepb_services.NotificationServiceServer
 )
