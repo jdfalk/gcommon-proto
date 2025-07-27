169866a9603487db1915b76321552040d05cad18
Johnathan Falk <johnathan.falk@gmail.com>
2025-07-27 08:00:26 -0400

    feat(notification): update proto files to include additional request and response types
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- pkg/notification/proto/proto.go
+++ pkg/notification/proto/proto.go
@@ -1,35 +1,9 @@
 // file: pkg/notification/proto/proto.go
 
 // Package notificationpb provides consolidated exports for notification protobuf types
+// All types are generated into this single package from the modular proto files
 package notificationpb
 
-// Re-export types from notification protobuf packages for backwards compatibility
-import (
-	notepb_enums "github.com/jdfalk/gcommon/pkg/notification/proto/enums"
-	notepb_messages "github.com/jdfalk/gcommon/pkg/notification/proto/messages"
-	notepb_requests "github.com/jdfalk/gcommon/pkg/notification/proto/requests"
-	notepb_responses "github.com/jdfalk/gcommon/pkg/notification/proto/responses"
-	notepb_services "github.com/jdfalk/gcommon/pkg/notification/proto/services"
-)
-
-// Message types
-type (
-	NotificationMessage       = notepb_messages.NotificationMessage
-	DeliveryChannel           = notepb_messages.DeliveryChannel
-	Template                  = notepb_messages.Template
-	SubscriptionPreferences   = notepb_messages.SubscriptionPreferences
-	EventNotification         = notepb_messages.EventNotification
-	SendNotificationRequest   = notepb_requests.SendNotificationRequest
-	UpdatePreferencesRequest  = notepb_requests.UpdatePreferencesRequest
-	GetTemplateRequest        = notepb_requests.GetTemplateRequest
-	SendNotificationResponse  = notepb_responses.SendNotificationResponse
-	UpdatePreferencesResponse = notepb_responses.UpdatePreferencesResponse
-	GetTemplateResponse       = notepb_responses.GetTemplateResponse
-)
-
-// Enum types
-type (
-	DeliveryStatus            = notepb_enums.DeliveryStatus
-	DeliveryChannelType       = notepb_messages.DeliveryChannelType
-	NotificationServiceServer = notepb_services.NotificationServiceServer
-)
+// This file serves as documentation and type organization for the generated protobuf types.
+// All types from the various .proto files in subdirectories are generated directly into this package.
+// No re-exports are needed since everything is already in the notificationpb package.
