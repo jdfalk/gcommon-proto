bc4d6c0c395640bd65ee67001ae4890b143f42f3
Johnathan Falk <johnathan.falk@gmail.com>
2025-08-04 06:14:09 -0400

    refactor(proto): remove unused log proto.go file as a placeholder
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- pkg/log/proto/proto.go
+++
@@ -1,7 +0,0 @@
-// file: pkg/log/proto/proto.go
-
-// Package logpb provides consolidated exports for log protobuf types
-package logpb
-
-// Since there are no generated .pb.go files in log proto subdirectories yet,
-// this package serves as a placeholder for future log service definitions.
