9b6cbda7b4490ec5ef0ebd49598582b31d034646
Johnathan Falk <johnathan.falk@gmail.com>
2025-08-04 06:13:51 -0400

    refactor(proto): remove unused health proto.go file as a placeholder
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- pkg/health/proto/proto.go
+++
@@ -1,7 +0,0 @@
-// file: pkg/health/proto/proto.go
-
-// Package healthpb provides consolidated exports for health protobuf types
-package healthpb
-
-// Since there are no generated .pb.go files in health proto subdirectories yet,
-// this package serves as a placeholder for future health service definitions.
