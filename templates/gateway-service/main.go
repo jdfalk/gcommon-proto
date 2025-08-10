c7eefa837a33e1c2beb3661f99decae1b9eaf1d4
Johnathan Falk <johnathan.falk@gmail.com>
2025-08-10 08:29:10 -0400

    feat(templates): add microservice scaffolding tools

---
+++ templates/gateway-service/main.go
@@ -0,0 +1,12 @@
+// file: templates/gateway-service/main.go
+// version: 1.0.0
+// guid: 0d18ebce-0e51-4482-9e2b-10a6bb8f4958
+
+// Package main provides an API gateway service template.
+package main
+
+import "log"
+
+func main() {
+	log.Println("gateway service starting")
+}
