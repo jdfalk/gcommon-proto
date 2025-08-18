f3dff6250a1828dfc949ed36b64437cf65370854
Johnathan Falk <johnathan.falk@gmail.com>
2025-08-18 19:57:18 -0400

    Remove unused gRPC example files and related interceptors
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- pkg/grpc/interceptors/doc.go
+++
@@ -1,6 +0,0 @@
-// file: pkg/grpc/interceptors/doc.go
-// version: 1.0.0
-// guid: 9460e750-6e93-41fb-8f27-d3b856f87571
-
-// Package interceptors contains reusable gRPC server interceptors.
-package interceptors
