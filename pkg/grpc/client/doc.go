b8d2a1524b7f8997be648b9b838347c0e25ad618
Johnathan Falk <johnathan.falk@gmail.com>
2025-08-18 19:57:08 -0400

    Remove unused gRPC client components and associated tests
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- pkg/grpc/client/doc.go
+++
@@ -1,6 +0,0 @@
-// file: pkg/grpc/client/doc.go
-// version: 1.0.0
-// guid: 902d05c0-709c-4996-bb36-9594dfee90d9
-
-// Package client provides utilities for managing gRPC client connections.
-package client
