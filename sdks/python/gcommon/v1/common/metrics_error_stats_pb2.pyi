1c44bce81c455916857614397e9c9b86a8e68d1c
Johnathan Falk <johnathan.falk@gmail.com>
2025-08-23 07:44:45 -0400

    feat(proto): add ErrorTypeCount and TimeRestriction messages with related updates in metrics and organization access control
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- sdks/python/gcommon/v1/common/metrics_error_stats_pb2.pyi
+++ sdks/python/gcommon/v1/common/metrics_error_stats_pb2.pyi
@@ -1,4 +1,4 @@
-from gcommon.v1.metrics import error_type_count_pb2 as _error_type_count_pb2
+from gcommon.v1.common import error_type_count_pb2 as _error_type_count_pb2
 from google.protobuf import go_features_pb2 as _go_features_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
--- sdks/python/gcommon/v1/common/organization_access_control_pb2.pyi
+++ sdks/python/gcommon/v1/common/organization_access_control_pb2.pyi
@@ -1,4 +1,4 @@
-from gcommon.v1.organization import time_restriction_pb2 as _time_restriction_pb2
+from gcommon.v1.common import time_restriction_pb2 as _time_restriction_pb2
 from google.protobuf import go_features_pb2 as _go_features_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
